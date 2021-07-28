f63659405f98c6ee53d49cd001a4d7e7ecf66ab4
Utkarsh Maken <utkarsh.maken@mavq.com>
2021-07-28 11:20:27 +0530

    Initial Project Add

---
+++ src/main/java/um/springframework/umpetclinic/UmPetClinicApplication.java
@@ -0,0 +1,13 @@
+package um.springframework.umpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/um/springframework/umpetclinic/UmPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package um.springframework.umpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
